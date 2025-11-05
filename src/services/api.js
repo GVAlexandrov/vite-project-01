57ba2c34ee085876ad8d2a96d129c8b432ae8f96
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2025-11-05 21:09:21 +0200

    2nd commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,14 @@
 const API_KEY = 'a0ebdb62f7476472f41f15db8b9c4ffc';
 const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie/?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
